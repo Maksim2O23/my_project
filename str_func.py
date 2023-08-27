5ca02c5e189ee002fe4c19ec0dfc900ed3bde4fb
Maksim2O23 <142991938+Maksim2O23@users.noreply.github.com>
2023-08-27 21:07:27 +0700

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def uppercase_string(input_string):
+    """
+    Преобразует входную строку в верхний регистр.
+
+    Args:
+        input_string (str): Входная строка для преобразования.
+
+    Returns:
+        str: Строка в верхнем регистре.
+    """
     return input_string.upper()
